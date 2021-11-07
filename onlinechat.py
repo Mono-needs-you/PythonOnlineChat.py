4de1b2243edce1d6d2e2ad768d18ffe02c99efa5
Mono-needs-you <55310057+ilnaz0000@users.noreply.github.com>
2021-11-07 17:08:36 +0300

    Add files via upload

---
+++ onlinechat.py
@@ -0,0 +1,99 @@
+#programm was maked by Mono_needs_you
+import asyncio
+
+from pywebio import start_server
+from pywebio.input import *
+from pywebio.output import *
+from pywebio.session import defer_call, info as session_info, run_async, run_js
+
+ch1 = input('Привет! выберите опцию!(1 = Лист доступных программ, 2 = обновить программу, 3 = выйти из программы) = ');
+if ch1 == '1':
+  pr1 = input('Выберите программу!(1 = BrowserChat, 2 = calculator ; 3 = выйти) = ');
+    if pr1 = '1':
+chat_msgs = []
+online_users = set()
+
+MAX_MESSAGES_COUNT = 100
+
+async def main():
+    global chat_msgskф
+    
+    put_markdown("## Привет! это онлайн чат написанный на python!\nОбщайтесь с удовольствием!")
+
+    msg_box = output()
+    put_scrollable(msg_box, height=300, keep_bottom=True)
+
+    nickname = await input("Войти в чат", required=True, placeholder="Ваше имя", validate=lambda n: "Такой ни    к уже используется!" if n in online_users or n == '📢' else None)
+    online_users.add(nickname)
+
+    chat_msgs.append(('📢', f'`{nickname}` присоединился к чату!'))
+    msg_box.append(put_markdown(f'📢 `{nickname}` присоединился к чату'))
+
+    refresh_task = run_async(refresh_msg(nickname, msg_box))
+
+    while True:
+        data = await input_group("💭 Новое сообщение", [
+            input(placeholder="Текст сообщения ...", name="msg"),
+            actions(name="cmd", buttons=["Отправить", {'label': "Выйти из чата", 'type': 'cancel'}])
+        ], validate = lambda m: ('msg', "Введите текст сообщения!") if m["cmd"] == "Отправить" and not m['msg'] else None)
+
+        if data is None:
+            break
+
+        msg_box.append(put_markdown(f"`{nickname}`: {data['msg']}"))
+        chat_msgs.append((nickname, data['msg']))
+
+    refresh_task.close()
+
+    online_users.remove(nickname)
+    toast("Вы вышли из чата!")
+    msg_box.append(put_markdown(f'📢 Пользователь `{nickname}` покинул чат!'))
+    chat_msgs.append(('📢', f'Пользователь `{nickname}` покинул чат!'))
+
+    put_buttons(['Перезайти'], onclick=lambda btn:run_js('window.location.reload()'))
+
+async def refresh_msg(nickname, msg_box):
+    global chat_msgs
+    last_idx = len(chat_msgs)
+
+    while True:
+        await asyncio.sleep(1)
+        
+        for m in chat_msgs[last_idx:]:
+            if m[0] != nickname: # if not a message from current user
+                msg_box.append(put_markdown(f"`{m[0]}`: {m[1]}"))
+        
+        # remove expired
+        if len(chat_msgs) > MAX_MESSAGES_COUNT:
+            chat_msgs = chat_msgs[len(chat_msgs) // 2:]
+        
+        last_idx = len(chat_msgs)
+
+if __name__ == "__main__":
+    start_server(main, debug=True, port=8080, cdn=False)
+    if pr1 = '2':
+    print("Ноль в качестве знака операции"
+      "\nзавершит работу программы")
+while True:
+    s = input("Знак (+,-,*,/): ")
+    if s == '0':
+        break
+    if s in ('+', '-', '*', '/'):
+        x = float(input("x="))
+        y = float(input("y="))
+        if s == '+':
+            print("%.2f" % (x+y))
+        elif s == '-':
+            print("%.2f" % (x-y))
+        elif s == '*':
+            print("%.2f" % (x*y))
+        elif s == '/':
+            if y != 0:
+                print("%.2f" % (x/y))
+            else:
+                print("Деление на ноль!")
+    else:
+        print("Неверный знак операции!")
+        if pr1 = '3':
+        exit
+
